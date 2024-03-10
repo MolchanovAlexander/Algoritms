713d625195494e4444fa50e1c34779ea19533d89
MolchanovAlexander <rybkolhoz@gmail.com>
2024-03-10 09:36:11 +0200

    pretty added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,23 @@ public class Main {
 //        System.out.println(LevenshteinDistance.distance("abcdef", "azcde"));
 //        int[] customers = new int[]{2, 2, 3, 3, 4, 4}; // if checkout - 2 - res - 9
 //        System.out.println(SupermarketQueue.calculateTotalTime(customers, 6));
-        Stack2<String> newStack = new Stack2();
-        System.out.println(newStack.isEmpty());
-        newStack.push("guf0");
-        newStack.push("guf1");
-        newStack.push("guf2");
-        newStack.push("guf3");
-        System.out.println(newStack.pop());
-        System.out.println(newStack.peek());
-        System.out.println(newStack.isEmpty());
-        System.out.println(newStack.size());
+        // STACK
+//        Stack2<String> newStack = new Stack2();
+//        System.out.println(newStack.isEmpty());
+//        newStack.push("guf0");
+//        newStack.push("guf1");
+//        newStack.push("guf2");
+//        newStack.push("guf3");
+//        System.out.println(newStack.pop());
+//        System.out.println(newStack.peek());
+//        System.out.println(newStack.isEmpty());
+//        System.out.println(newStack.size());
+        // pretty array
+        int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7}; // pretty
+        int[] arr2 = new int[]{1, 2, 2, 2, 2, 2}; // pretty
+        int[] arr3 = new int[]{0, 2, 3, 4, 5, 6, 7}; // not pretty
+        System.out.println(PrettyArray.isArrayPretty(arr2));
+        System.out.println(PrettyArray.isBadunPretty(arr3));
 
     }
 }
---
+++ src/main/java/org/example/PrettyArray.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class PrettyArray {
+    public static boolean isArrayPretty(int[] array) {
+        int countOfMismatch = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] != array[j] - 1 && array[i] != array[j] + 1) {
+                    countOfMismatch++;
+                }//find at least 1 pretty member from all array
+            }
+            if (countOfMismatch == array.length) {
+                return false; // if no one found return ;
+            }
+            countOfMismatch = 0; // preparing for new cycle
+        }
+        return true;
+    }
+
+    public static boolean isBadunPretty(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j] - 1 || array[i] == array[j] + 1) {
+                    count++; //find at least 1 pretty member from all array
+                    break; // then break cycle;
+                }
+            }
+        } //find at least 1 pretty member for all other members
+        return count == array.length;
+    }
+}
