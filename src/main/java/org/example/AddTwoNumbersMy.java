627ddc3bcb57ca60838aa9e8933e99f61981a28e
OlleksandrMolchanov <rybkolhoz@gmail.com>
2024-03-22 14:49:13 +0200

    add two numbers style

--- src/main/java/org/example/AddTwoNumbersMy.java
+++ src/main/java/org/example/AddTwoNumbersMy.java
@@ -9,18 +9,15 @@ class AddTwoNumbersMy {
 
     public static Node addTwoNumbers(Node list1, Node list2) {
         top = null;
-        // System.out.println(list1.data + " " + list1.next.data + " " + list2.data + " " + list2.next.data);
         first = list1;
         second = list2;
         LinkedList<Integer> fres = new LinkedList<>();
         while (first != null) {
-            // System.out.println(top.data + " " + first.data + " count-" + count++);
             fres.add(first.data);
             first = first.next;
         }
         LinkedList<Integer> sres = new LinkedList<>();
         while (second != null) {
-            //System.out.println(top.data + " " + second.data + " count-" + count++);
             sres.add(second.data);
             second = second.next;
         }
@@ -32,15 +29,12 @@ class AddTwoNumbersMy {
         for (int i : sres) {
             sum2.append(i);
         }
-        System.out.println(sum1 + " " + sum2);
-        int res = Integer.parseInt(sum1.reverse().toString()) + Integer.parseInt(sum2.reverse().toString());
+        int res = Integer.parseInt(sum1.reverse().toString())
+                + Integer.parseInt(sum2.reverse().toString());
         char[] nextValue = String.valueOf(res).toCharArray();
         for (char ch : nextValue) {
             push(ch - '0');
         }
-        System.out.println(fres);
-        System.out.println(sres);
-        System.out.println(res);
         return top;
     }
 
