d5fe791503a77a162bc42df0ba13610254a09262
MolchanovAlexander <rybkolhoz@gmail.com>
2024-03-22 15:29:50 +0200

    refactored

--- src/main/java/org/example/AddTwoNumbers.java
+++ src/main/java/org/example/AddTwoNumbers.java
@@ -1,23 +1,29 @@
 package org.example;
 
-public class AddTwoNumbers {
-  public static Node addTwoNumbers(Node list1, Node list2) {
-    int carry = 0;
-    Node pos, current = new Node(0);
-    pos = current;
-    while (list1 != null || list2 != null || carry != 0) {
-      if (list1 != null) {
-        carry += list1.data;
-        list1 = list1.next;
-      }
-      if (list2 != null) {
-        carry += list2.data;
-        list2 = list2.next;
-      }
-      pos.next = new Node(carry % 10);
-      carry /= 10;
-      pos = pos.next;
+class AddTwoNumbers {
+    public static Node top;
+
+    public static Node addTwoNumbers(Node list1, Node list2) {
+        top = null;
+        int res = process(list1) + process(list2);
+        char[] nextValue = String.valueOf(res).toCharArray();
+        for (char ch : nextValue) {
+            push(ch - '0');
+        }
+        return top;
     }
-    return current.next;
-  }
+    public static int process(Node node) {
+        StringBuilder sum1 = new StringBuilder();
+        while (node != null) {
+            sum1.append(node.data);
+            node = node.next;
+        }
+       return Integer.parseInt(sum1.reverse().toString());
+    }
+
+    public static void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
 }
