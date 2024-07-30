44db0b10c11148b52484d7214e1ac18a94d70f41
MolchanovAlexander <rybkolhoz@gmail.com>
2024-07-30 10:09:33 +0300

    comparator

--- src/main/java/org/example/comparator/Main.java
+++ src/main/java/org/example/comparator/Main.java
@@ -5,7 +5,8 @@ import java.util.PriorityQueue;
 public class Main {
     public static void main(String[] args) {
         // Create a PriorityQueue with the ReversePersonAgeComparator
-        PriorityQueue<Person> pq = new PriorityQueue<>(new ReversePersonAgeComparator());
+        PriorityQueue<Person> pq = new PriorityQueue<>((p1, p2) ->Integer.compare(p2.getAge(), p1.getAge()));
+        //PriorityQueue<Person> pq = new PriorityQueue<>(new PersonAgeComparator());
 
         pq.add(new Person("Alice", 30));
         pq.add(new Person("Bob", 25));
